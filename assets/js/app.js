e19d928a7ff90085cf5e1c3d469b97b19b7e31d2
Anik Datta <anikdatta50@gmail.com>
2022-10-19 00:09:07 +0600

    Strict psd to html

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'wave',
+   
+});
+
+new VenoBox({
+    selector: '.my-custom-links',
+    spinner: 'wave',
+});
